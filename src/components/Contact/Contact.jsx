4fa746b8355855ed7e5df88ebea043ff546c3d8b
TatiYas <iasinovska_lawyer@ukr.net>
2024-08-18 15:38:09 +0200

    Update_8

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,62 +1,32 @@
 import { useDispatch } from "react-redux";
-import { useState } from "react";
-import { deleteContact } from "../../redux/contacts/operations";
-import { BsPersonFill } from "react-icons/bs";
-import { HiPhone } from "react-icons/hi2";
-import Stack from "@mui/material/Stack";
-import IconButton from "@mui/material/IconButton";
-import EditIcon from "@mui/icons-material/Edit";
-import DeleteIcon from "@mui/icons-material/Delete";
-import EditModal from "../EditModal/EditModal";
 import css from "./Contact.module.css";
-import DelModal from "../DelModal/DelModal";
+import { deleteContact } from "../../redux/contactsOps";
+import { BsFillTelephoneFill, BsFillPersonFill } from "react-icons/bs";
 
-export default function Contact({ data: { id, name, number } }) {
+export const Contact = ({ contact: { id, name, number } }) => {
   const dispatch = useDispatch();
-  const [isModalOpen, setModalOpen] = useState(false);
-  const [delModalOpen, setDelModalOpen] = useState(false);
-
-  const handleEditClick = () => {
-    setModalOpen(true);
-  };
-
-  const handleDeleteClick = () => {
-    setDelModalOpen(true);
-  };
+  const handleDelete = () => dispatch(deleteContact(id));
 
   return (
-    <div className={css.container}>
+    <>
       <div>
-        <p>
-          <BsPersonFill className={css.myIcon} size="20" />
-          {name}
-        </p>
-        <p>
-          <HiPhone className={css.myIcon} size="20" />
-          {number}
-        </p>
+        <div className={css.element}>
+          <span className={css.icon}>
+            <BsFillPersonFill size={30} color="black" />
+          </span>
+          <p className={css.text}>{name}</p>
+        </div>
+        <div className={css.element}>
+          <span className={css.icon}>
+            <BsFillTelephoneFill size={30} color="black" />
+          </span>
+          <p className={css.text}>{number}</p>
+        </div>
       </div>
-      <Stack direction="row">
-        <IconButton aria-label="edit" onClick={handleEditClick}>
-          <EditIcon className={css.myIcon} />
-        </IconButton>
-        <IconButton aria-label="delete" onClick={handleDeleteClick}>
-          <DeleteIcon className={css.myIcon} />
-        </IconButton>
-      </Stack>
-      <EditModal
-        open={isModalOpen}
-        handleClose={() => setModalOpen(false)}
-        initialValues={{ id, name, number }}
-      />
-      <DelModal
-        open={delModalOpen}
-        close={() => setDelModalOpen(false)}
-        handleDelete={() => {
-          dispatch(deleteContact(id));
-        }}
-        contactName={name}
-      />
-    </div>
+
+      <button className={css.btn} onClick={handleDelete} type="submit">
+        Delete
+      </button>
+    </>
   );
-}
+};
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,9 +3,10 @@ import css from "./HomePage.module.css";
 export default function HomePage() {
   return (
     <div className={css.container}>
-      <h1 className={css.title}>Contact book for close people </h1>
+      <h1 className={css.title}>Contact book </h1>
       <p className={css.text}>
-        which of your friends will say [palyanytsia] correctly?
+        which of your friends 
+        really brave?
       </p>
     </div>
   );
